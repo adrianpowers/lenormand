07d7d2952e78c44b50442e176b96570ed8578c33
Katya <kittyserenakat@gmail.com>
2022-11-05 00:17:21 -0400

    Add select card functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,12 +13,18 @@ class App extends Component {
       card1: {},
       card2: {},
       isFlipped: false,
-      leftCardMeaning: '',
-      rightCardMeaning: ''
+      isStarting: true
     }
   }
+
+  onCardClick = (event) => {
+    const cardId = event.target.id;
+    const pickedCard = cards.filter(card => card.number === cardId)
+    this.setState({card1: pickedCard[0], isStarting: false})
+  }
+
   render(){
-    // const filteredDeck = [];
+    const filteredDeck = [];
 
     return (
       <div>
@@ -27,8 +33,9 @@ class App extends Component {
         {/* <p>In the Lenormand system, two card spreads are drawn. The left card is the adjective, while the right card is the noun. The order can drastically change the reading interpretation.</p>
         <h2>INSTRUCTIONS: Pick your first card / Pick your second card.</h2> */}
         
-        <Deck filteredDeck={cards} />
-        <h2>Pick your (first/second) card</h2>
+        <Deck filteredDeck={cards} onCardClick={this.onCardClick} />
+        <h2>Pick your {this.state.isStarting ? 'first' : 'second'} card</h2>
+        <h2>You picked {this.state.card1.name} </h2>
         <Hand />
 
         {/* <p>SHOW ALL CARDS HERE. When they pick their cards, put them away in a stack to the side and replace with two cards.</p>
