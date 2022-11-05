ae335ca71f56877b87d367bdeda373d48938a6a2
Katya <kittyserenakat@gmail.com>
2022-11-05 01:40:33 -0400

    Add array shuffle states

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -5,13 +5,40 @@ import backOfCardDark from '../../images/back03.jpg'
 
 const Deck = ({filteredDeck, onCardClick}) => {
     const laidCards = filteredDeck.map( (card, index) => {
-
+        
         const randomNum = Math.floor(Math.random() * (10 - -10) -10 )
 
         if(index % 2 === 0) {
-            return <div className='cardContainer' style={{transform: `rotate(${randomNum}deg)` }}><img src={backOfCard} alt="back of light card" key={card.number} id={card.number} className='deckImage' onClick={onCardClick} /></div>
+            return (
+                <div 
+                    className='cardContainer' 
+                    style={{transform: `rotate(${randomNum}deg)` }} 
+                    key={card.number}
+                >
+                    <img 
+                        src={backOfCard} 
+                        alt="back of light card" 
+                        id={card.number} 
+                        className='deckImage' 
+                        onClick={onCardClick} 
+                    />
+                </div>)
         } else {
-            return <div className='cardContainer' style={{transform: `rotate(${randomNum}deg)` }}><img src={backOfCardDark} alt="back of dark card" key={card.number} id={card.number} className='deckImage' onClick={onCardClick} /></div>
+            return (
+                <div 
+                    className='cardContainer' 
+                    style={{transform: `rotate(${randomNum}deg)` }} 
+                    key={card.number}
+                >
+                    <img 
+                        src={backOfCardDark} 
+                        alt="back of dark card" 
+                        id={card.number} 
+                        className='deckImage' 
+                        onClick={onCardClick} 
+                    />
+                </div>
+            )
         }
     })
     return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import cards from '../cards'
 import Deck from '../components/Deck/Deck';
 import Hand from '../components/Hand/Hand';
+// import Shuffle from '../components/Shuffle/Shuffle';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      deck: {},
+      shuffledDeck: [],
       card1: {},
       card2: {},
       isFlipped: false,
@@ -21,25 +22,73 @@ class App extends Component {
     const cardId = event.target.id;
     const pickedCard = cards.filter(card => card.number === cardId)
     this.setState({card1: pickedCard[0], isStarting: false})
+    console.log("ON CARD CLICK =================================================================")
+    console.log(pickedCard);
+    console.log(this.state.shuffledDeck);
+  }
+
+  shuffle = (deck) => {
+    let currDeck = [...deck];
+    let currentIndex = currDeck.length,  randomIndex;
+
+    while (currentIndex != 0) {
+
+      // Pick a remaining card.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current card.
+      [
+        currDeck[currentIndex], 
+        currDeck[randomIndex]
+      ] = [
+        currDeck[randomIndex], 
+        currDeck[currentIndex]
+      ];
+    }
+    return currDeck;
+  }
+
+  shuffleHandler = () => {
+    const newShuffle = this.shuffle(this.state.shuffledDeck)
+    this.setState({shuffledDeck: newShuffle});
+    console.log("ON CLICK =================================================================")
+    console.log(this.state.shuffledDeck)
+  }
+  
+  componentDidMount(){
+    const shuffledCards = this.shuffle(cards);
+    this.setState({
+      shuffledDeck: [...shuffledCards]
+    })
+    console.log("ON MOUNT =================================================================")
+    console.log(this.state.shuffledDeck);
   }
 
   render(){
-    const filteredDeck = [];
-
-    return (
+    console.log("ON RENDER =================================================================")
+    console.log(this.state.shuffledDeck);
+    return(
       <div>
-        {/* <p>{cards[0].name}</p> */}
         <h1> Lenormand Reading </h1>
-        {/* <p>In the Lenormand system, two card spreads are drawn. The left card is the adjective, while the right card is the noun. The order can drastically change the reading interpretation.</p>
-        <h2>INSTRUCTIONS: Pick your first card / Pick your second card.</h2> */}
-        
-        <Deck filteredDeck={cards} onCardClick={this.onCardClick} />
+
+        <button 
+          id="shuffleButton" 
+          onClick={this.shuffleHandler}
+        >
+          <h2>Shuffle Deck</h2>
+        </button>
+
+        <Deck 
+          filteredDeck={this.state.shuffledDeck} 
+          onCardClick={this.onCardClick} 
+        />
+
         <h2>Pick your {this.state.isStarting ? 'first' : 'second'} card</h2>
         <h2>You picked {this.state.card1.name} </h2>
+
         <Hand />
 
-        {/* <p>SHOW ALL CARDS HERE. When they pick their cards, put them away in a stack to the side and replace with two cards.</p>
-        <p>FLIP TWO CARDS. Show images of the cards from that website or store the images?</p> */}
       </div>
     );
   }
